f8437f17a71f0b719f6594e58c606092d2e683fe
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-09-23 11:21:53 +0530

    add feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+
+const relatives = ["Raj", "David", "Lily", "Edwar", "Rebecca"];
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key="relativeList">
+        {relatives.map((relative, i) => {
+          return <li key={`relativeListItem${i + 1}`}>{relative}</li>;
+        })}
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
